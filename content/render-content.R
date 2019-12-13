9e6c72409d5b92736ed4293b469e8261dbf3a68b
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2019-12-13 20:18:43 +0100

    Added messages and forced render in render-content

--- content/render-content.R
+++ content/render-content.R
@@ -3,10 +3,17 @@ library("knitr")
 # Get all source .Rmd files and all .md files
 rmd_files <- list.files("./content", pattern = "\\.Rmd$", recursive = TRUE,
                         full.names = TRUE, ignore.case = TRUE)
+force_knit <- TRUE
 
 # Run through all files
 for (f in rmd_files)  {
-
+  o <- gsub(".Rmd", ".md", f)
+  if (file.exists(o) && !force_knit) {
+    message("Found:    ", o)
+    message("Skipping: ", f)
+    next
+  }
+  
   # Create folder for the particular Rmd file
   f_noext <- tools::file_path_sans_ext(basename(f))
   img_dir <- file.path("./static/images", f_noext, "/")
@@ -15,6 +22,10 @@ for (f in rmd_files)  {
   # Set newly created img_dir as figure path
   opts_chunk$set(fig.path = img_dir)
 
-  knitr::knit(input = f, output = gsub(".Rmd", ".md", f))
+  knitr::knit(input = f, output = o)
+  
+  # Correcting image paths in markdown
+  message("Correcting image paths ('./static/images/...' -> '/images/...')")
+  writeLines(gsub("./static/images", "/images", readLines(o), fixed = TRUE),
+             con = o)
 }
-
