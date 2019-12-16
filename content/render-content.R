633601fb8b9fd69e031387d876a7872a8f6a7457
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2019-12-16 20:30:40 +0100

    Added automatic language copy

--- content/render-content.R
+++ content/render-content.R
@@ -1,19 +1,39 @@
 library("knitr")
 
 # Get all source .Rmd files and all .md files
-rmd_files <- list.files("./content", pattern = "\\.Rmd$", recursive = TRUE,
-                        full.names = TRUE, ignore.case = TRUE)
-force_knit <- FALSE
+# Need to be 're-run' to get new generated files (if any)
 
-# Run through all files
-for (f in rmd_files)  {
+rmd_files <- function() {
+  list.files("./content", pattern = "\\.Rmd$", recursive = TRUE,
+             full.names = TRUE, ignore.case = TRUE)
+}
+
+force_knit <- TRUE
+
+# Copy those with no danish version
+comment <- "<!---Automatically generated from 'render-content.R'--->"
+for (f in rmd_files()) {
+  da_f <- gsub("\\.en\\.", ".da.", f)
+  # Write if not existing or already generated
+  if (!file.exists(da_f) || any(comment == readLines(da_f))) {
+    if (file.exists(da_f)) {
+      Sys.chmod(da_f, mode = "0777")
+    }
+    lns <- c(readLines(f), comment)
+    writeLines(lns, con = da_f)
+    Sys.chmod(da_f, mode = "0444")
+  }
+}
+
+# Run through all files to knit
+for (f in rmd_files())  {
   o <- gsub(".Rmd", ".md", f)
+  
   if (file.exists(o) && !force_knit) {
     message("Found:    ", o)
     message("Skipping: ", f)
     next
   }
-  
   # Create folder for the particular Rmd file
   f_noext <- tools::file_path_sans_ext(basename(f))
   img_dir <- file.path("./static/images", f_noext, "/")
