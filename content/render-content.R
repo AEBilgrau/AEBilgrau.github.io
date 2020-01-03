2e86ec1b433b2ed57eea4f4216190090ef3a6151
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2020-01-03 20:45:29 +0100

    Copy only post when no danish version is available. Added generated content from Rmd.

--- content/render-content.R
+++ content/render-content.R
@@ -10,21 +10,6 @@ rmd_files <- function() {
 
 force_knit <- TRUE
 
-# Copy those with no danish version
-comment <- "<!---Automatically generated from 'render-content.R'--->"
-for (f in rmd_files()) {
-  da_f <- gsub("\\.en\\.", ".da.", f)
-  # Write if not existing or already generated
-  if (!file.exists(da_f) || any(comment == readLines(da_f))) {
-    if (file.exists(da_f)) {
-      Sys.chmod(da_f, mode = "0777")
-    }
-    lns <- c(readLines(f), comment)
-    writeLines(lns, con = da_f)
-    Sys.chmod(da_f, mode = "0444")
-  }
-}
-
 # Run through all files to knit
 for (f in rmd_files())  {
   o <- gsub(".Rmd", ".md", f)
@@ -48,4 +33,21 @@ for (f in rmd_files())  {
   message("Correcting image paths ('./static/images/...' -> '/images/...')")
   writeLines(gsub("./static/images", "/images", readLines(o), fixed = TRUE),
              con = o)
+}
+
+# Copy those with no danish version
+comment <- "<!---Automatically generated from 'render-content.R'--->"
+for (f in rmd_files()) {
+  en_md <- gsub("\\.en\\.Rmd$", ".en.md", f)
+  da_md <- gsub("\\.en\\.Rmd$", ".da.md", f)
+  # Write if not existing or already generated
+  if ((!file.exists(da_md) && file.exists(en_md)) ||
+      any(comment == readLines(da_md))) {
+    if (file.exists(da_md)) {
+      Sys.chmod(da_md, mode = "0777")
+    }
+    lns <- c(readLines(en_md), comment)
+    writeLines(lns, con = da_md)
+    Sys.chmod(da_md, mode = "0444")
+  }
 }
